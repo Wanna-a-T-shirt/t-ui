b20e30ed70c6ecbd093dca61560087fcadbb26a2
Phoebe <597282659@qq.com>
2020-09-17 15:06:17 +0800

    add click

--- t-ui/src/app/chart-container/chart-container.component.ts
+++ t-ui/src/app/chart-container/chart-container.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as echarts from 'echarts';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
@@ -8,12 +8,14 @@ import 'echarts/lib/component/toolbox';
 @Component({
   selector: 'app-chart-container',
   templateUrl: './chart-container.component.html',
-  styleUrls: ['./chart-container.component.less']
+  styleUrls: ['./chart-container.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ChartContainerComponent implements OnInit {
-
   private nodeData: any[];
-  links: any[] = [];
+  private links: any[] = [];
+  displayInfo: boolean = false;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -78,9 +80,23 @@ export class ChartContainerComponent implements OnInit {
   };
     lineChart.setOption(option);
 
+    lineChart.on('click', (param) => {
+        console.log('param---->', param);  // 打印出param, 可以看到里边有很多参数可以使用
+        // 获取节点点击的数组序号
+        let arrayIndex = param.dataIndex;
+        console.log('arrayIndex---->', arrayIndex);
+        console.log('name---->', param.name);
+        if (param.dataType === 'node') {
+            alert("clicked node" + param.name);
+            this.displayInfo = true;
+        } else {
+            alert("clicked arrow" + param.value);
+        }
+    });
+
   }
 
-  calculateLinks(relationShips: any[]) {
+  private calculateLinks(relationShips: any[]) {
     relationShips.forEach(relationShip => {
         let source = relationShip.componentId;
         relationShip.downstreamComponentIds.forEach(downStream => {
